289bd08ee3712a1681047cf49360f2e6ddf560fe
100gun <xxxxgunshot@gmail.com>
2025-06-19 16:28:56 +0900

    ♻️ 404 > inline onclick 이벤트 제거 및 코드 수정

--- js/404.js
+++ js/404.js
@@ -1,8 +1,14 @@
 console.log("404 Error: Page not found -", window.location.href);
 
 // 뒤로가기 버튼
-const backButton = document.querySelector(".btn-secondary");
-if (backButton && window.history.length <= 1) {
-  backButton.textContent = "이전 페이지";
-  backButton.onclick = () => (window.location.href = "/");
+const backButton = document.getElementById("backButton");
+
+if (backButton) {
+  backButton.addEventListener("click", function () {
+    if (window.history.length <= 1) {
+      window.location.href = "/";
+    } else {
+      window.history.back();
+    }
+  });
 }
