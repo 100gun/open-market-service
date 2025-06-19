8d184bf55b8dda7042b320a8176885dde0b98345
100gun <xxxxgunshot@gmail.com>
2025-06-19 12:53:42 +0900

    📝 config.js 추가

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const API_BASE_URL = "https://api.wenivops.co.kr/services/open-market";
+
+export const API_ENDPOINTS = {
+  PRODUCTS: `${API_BASE_URL}/products/`,
+  LOGIN: `${API_BASE_URL}/accounts/login/`,
+};
