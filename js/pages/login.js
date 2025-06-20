3db812c094862d573a23d3cc168a5cd7b9fd6dc6
100gun <xxxxgunshot@gmail.com>
2025-06-21 01:42:53 +0900

    📝 로그인 > 헤더 아이콘이 바뀌지 않는 오류 수정중

--- js/pages/login.js
+++ js/pages/login.js
@@ -143,6 +143,8 @@ function handleLoginSuccess(data) {
 
   window.dispatchEvent(new CustomEvent("loginSuccess", { detail: user }));
 
+  updateHeader();
+
   alert(`${user.name}님, 로그인 성공!`);
 
   if (document.referrer && document.referrer !== window.location.href) {
