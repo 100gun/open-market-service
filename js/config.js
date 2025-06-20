6fe207520565d35991b10d5c3c2ae7de97da25e9
100gun <xxxxgunshot@gmail.com>
2025-06-20 15:08:27 +0900

    📝 config.js > detail 엔드포인트 추가

--- js/config.js
+++ js/config.js
@@ -3,4 +3,6 @@ export const API_BASE_URL = "https://api.wenivops.co.kr/services/open-market";
 export const API_ENDPOINTS = {
   PRODUCTS: `${API_BASE_URL}/products/`,
   LOGIN: `${API_BASE_URL}/accounts/login/`,
+  PRODUCT_DETAIL:
+    "https://api.wenivops.co.kr/services/open-market/products/<int:product_id>/",
 };
